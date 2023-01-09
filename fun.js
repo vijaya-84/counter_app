2103beda8dab7748ff4a910d846c62e7aa9fe935
vijaya_@ <118622957+vijaya-84@users.noreply.github.com>
2023-01-09 18:43:57 +0530

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,18 @@
+var m=0;
+function doAction(){
+
+ ++m;
+document.getElementById('number').value=m ;
+}
+
+
+function resetValue(m){
+      m=0
+    document.getElementById('number').value=m ;
+    
+}
+
+
+
+  
+
